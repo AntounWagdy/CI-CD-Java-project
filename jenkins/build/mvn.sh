f5e71c1e5656f2afa551a8194ccb0073378a14c3
user <antoun@git.com>
2021-02-03 16:17:27 +0200

    Testing hook

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,6 +3,6 @@ echo "************************"
 echo "** Building JAR *******"
 echo "************************"
 
-# $@ means all the parameters concatenated 
+# $@ means all parameters are concatenated 
 WDR="/media/antoun/0E4907C20E4907C2/Courses/[DesireCourse.Net] Udemy - Jenkins, From Zero To Hero Become a DevOps Jenkins Master/project/pipeline"
 docker run --rm -v "$WDR/java_app":/app -v "$WDR/maven-plugins":/root/.m2 -w /app maven:3-alpine "$@"
