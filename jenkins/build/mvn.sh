f946cd47a71a308b60aee0ecdba0e4514d196360
user <antoun@git.com>
2021-02-03 14:49:15 +0200

    Solving bash bugs

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -5,6 +5,5 @@ echo "************************"
 
 
 # $@ means all the parameters concatenated 
-$WDR = "/media/antoun/0E4907C20E4907C2/Courses/[DesireCourse.Net] Udemy - Jenkins, From Zero To Hero Become a DevOps Jenkins Master/project/pipeline"
-
+WDR="/media/antoun/0E4907C20E4907C2/Courses/[DesireCourse.Net] Udemy - Jenkins, From Zero To Hero Become a DevOps Jenkins Master/project/pipeline"
 docker run --rm -v "$WDR/java_app":/app -v "$WDR/maven-plugins":/root/.m2 -w /app maven:3-alpine "$@"
