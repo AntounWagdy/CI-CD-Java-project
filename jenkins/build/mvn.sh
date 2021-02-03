9699dbc158336fbfa79119944f53031e6fe4692d
user <antoun@git.com>
2021-02-03 14:27:32 +0200

    removing ti option

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,4 +6,4 @@ echo "************************"
 
 # $@ means all the parameters concatenated 
 
-docker run --rm -ti -v "$PWD/java_app":/app -v "$PWD/maven-plugins":/root/.m2 -w /app maven:3-alpine "$@"
+docker run --rm -v "$PWD/java_app":/app -v "$PWD/maven-plugins":/root/.m2 -w /app maven:3-alpine "$@"
